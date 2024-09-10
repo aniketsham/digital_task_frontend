0cfa9e3e7224c3072ba3e1c8f361ed85ca8026d9
Aniket Sharma <89375574+aniketsham@users.noreply.github.com>
2024-09-10 20:33:39 +0530

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -50,7 +50,8 @@ const Register = () => {
   
   return (
     <div>
-loading ? ( <div className='flex items-center justify-center h-screen'>
+      {
+      loading ? ( <div className='flex items-center justify-center h-screen'>
 
         <div role="status">
         <svg aria-hidden="true" class="inline w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -103,9 +104,7 @@ loading ? ( <div className='flex items-center justify-center h-screen'>
 )
 
 
-
-
-
+      }
 
 
     </div>
