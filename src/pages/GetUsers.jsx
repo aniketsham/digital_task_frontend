dfd27b4a3227be5ea4725ab0c4856d14ef9556b2
Aniket Sharma <89375574+aniketsham@users.noreply.github.com>
2024-09-10 20:32:32 +0530

    Update GetUsers.jsx

--- src/pages/GetUsers.jsx
+++ src/pages/GetUsers.jsx
@@ -49,7 +49,10 @@ const GetUsers = () => {
       }
 
   return (
-    loading ? ( <div className='flex items-center justify-center h-screen'>
+      <div>
+          {
+
+              loading ? ( <div className='flex items-center justify-center h-screen'>
 
         <div role="status">
         <svg aria-hidden="true" class="inline w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -128,6 +131,9 @@ const GetUsers = () => {
     </div>
     
     )
+          }
+      </div>
+    
 
 
 
